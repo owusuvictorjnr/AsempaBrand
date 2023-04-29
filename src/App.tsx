87c3082c1c1406a80b3dc83b7dafe1daef0082a4
owusuvictorjnr <owusuvictor80@gmail.com>
2023-04-29 03:53:22 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { sampleProduct } from './data';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>asempaBrand</header>
+        <main>
+          <ul>
+            {sampleProduct.map((product) => (
+              <li>
+                <img
+                  src={product.images}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>&#8373; {product.price}</p>
+              </li>
+            ))}
+          </ul>
+        </main>
+        <footer>all right reserved</footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
