f58122dd10f4f9ca7aa5a77f88ba040edf69a32b
owusuvictorjnr <owusuvictor80@gmail.com>
2023-05-14 14:36:00 +0000

    cart types

---
+++ src/types/Cart.ts
@@ -0,0 +1,27 @@
+export type CartItem = {
+  images: string | undefined;
+  slug: string;
+  quantity: number;
+  countInStock: number;
+  price: number;
+  _id: string;
+  name: string;
+};
+
+export type ShippingAddress = {
+  fullName: string;
+  address: string;
+  city: string;
+  country: string;
+  postCode: string;
+};
+
+export type Cart = {
+  itemsPrice: number;
+  shippingPrice: number;
+  taxPrice: number;
+  totalPrice: number;
+  cartItems: CartItem[];
+  shippingAddress: ShippingAddress;
+  paymentMethod: string;
+};
