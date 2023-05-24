64922e315ab9fb46bfec2c038616999689d3bd79
owusuvictorjnr <owusuvictor80@gmail.com>
2023-05-24 06:53:46 +0000

    create signup route

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StoreProvider } from './Store';
 import CartPage from './pages/CartPage';
 import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
       <Route path="product/:slug" element={<ProductPage />} />
       <Route path="cart" element={<CartPage />} />
       <Route path="signin" element={<SigninPage />} />
+      <Route path="signup" element={<SignupPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
